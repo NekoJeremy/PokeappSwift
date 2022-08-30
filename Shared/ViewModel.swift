1f3d14674932c10e75612ed8059fd99da9a3e11d
Jeremy Andrades <68261645+NekoJeremy@users.noreply.github.com>
2022-08-30 12:58:03 -0400

    getpokemon estable

--- Shared/ViewModel.swift
+++ Shared/ViewModel.swift
@@ -11,17 +11,18 @@ import SwiftUI
 struct PokemonDataModel : Decodable {
     let name: String
     let url: String
-    let frontDefault: String
+    /*let frontDefault: String?
 
     enum CodingKeys : String, CodingKey {
-        case urlDescription : "url"
+        case urlDescription = "url"
     }
 
     init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
         let description = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .urlDescription)
         print("description is here -------", description)
-        self.frontDefault = try description.decode(String.self, forKey: .urlDescription)
-    }
+        self.frontDefault = try? description.decode(String.self, forKey: .urlDescription)
+    }*/
 }
 
 struct PokemonResponseDataModel : Decodable {
@@ -29,17 +30,13 @@ struct PokemonResponseDataModel : Decodable {
     
     enum CodingKeys : String, CodingKey {
         case results
-        case urlDescription : "url"
 
     }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.pokemons = try container.decode([PokemonDataModel].self, forKey: .results)
-        let description = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .urlDescription)
-        print("description is here -------", description)
         print(pokemons)
-        self.frontDefault = try 
     }
 }
 
@@ -51,35 +48,22 @@ final class ViewModel : ObservableObject {
     var offset = 0
     
     func getPokemons() {
-        let urlSession = URLSession.shared
         let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit="+limit.codingKey.stringValue+"&amp,;offset="+offset.codingKey.stringValue)!
-        urlSession.dataTask(with: url) { data, response, error in
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
             if let _ = error {
-                print("Error en getPokemonList")
+                print("Error en getPokemons")
             }
             
+            let httpResponse = response as! HTTPURLResponse
+            
             if let data = data,
-               let httpResponse = response as? HTTPURLResponse,
                httpResponse.statusCode == 200 {
-                let pokemonDataModel = try JSONDecoder().decode(PokemonResponseDataModel.self, from: data)
+                let pokemonDataModel = try! JSONDecoder().decode(PokemonResponseDataModel.self, from: data)
                 DispatchQueue.main.async {
                     self.pokemons = pokemonDataModel.pokemons
-<<<<<<< HEAD:Shared/ListViewModel.swift
-                    //print("species ------- "+[pokemonDataModel].species)
                 }
-=======
->>>>>>> parent of fbb5c8f (change viewModel to list):Shared/ViewModel.swift
-                }
+            }
         }.resume()
     }
-<<<<<<< HEAD:Shared/ListViewModel.swift
-
-    /*
-    func getPokemonDescription() {
-        let index = offset + [pokemonDataModel].species
-        let urlDescription = URL(string: "https://pokeapi.co/api/v2/pokemon-species/"+index)
-    }
-    */
-=======
->>>>>>> parent of fbb5c8f (change viewModel to list):Shared/ViewModel.swift
 }
