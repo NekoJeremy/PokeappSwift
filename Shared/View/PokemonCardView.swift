8cb1927d0ede9dd80973475de8b22c7968fe2c04
Jeremy Andrades <jeremyandrades@gmail.com>
2022-09-07 21:12:55 -0300

    Update PokemonCardView.swift

--- Shared/View/PokemonCardView.swift
+++ Shared/View/PokemonCardView.swift
@@ -26,13 +26,14 @@ struct PokemonCardView: View {
                     .padding(.top, 8)
                     .padding(.leading)
                 HStack {
-                    Text("pokemon.type")
+                    Text(pokemon.type)
                         .foregroundColor(.white)
                         .font(.subheadline).bold()
                         .padding(.horizontal, 16)
                         .padding(.vertical, 8)
                         .overlay(
-                        RoundedRectangle(cornerRadius: 20)                            .fill(Color.white.opacity(0.25))
+                        RoundedRectangle(cornerRadius: 20)                            
+                        .fill(Color.white.opacity(0.25))
                         )
                         .frame(width: 100, height: 24, alignment: .leading)
                     Image("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png")
